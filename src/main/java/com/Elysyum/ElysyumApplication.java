d85ec408f8cad0bbc428c0d12166a0a4c88b8354
lascu <lascumihai31@gmai>
2023-01-14 13:46:01 +0200

    test

---
+++ src/main/java/com/Elysyum/ElysyumApplication.java
@@ -0,0 +1,13 @@
+package com.Elysyum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElysyumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElysyumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Elysyum/ElysyumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Elysyum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElysyumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
